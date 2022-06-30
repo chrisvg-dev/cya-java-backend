26becdcbe9cbb372ed2d0baa72821f9b0c202d97
CRISTHIAN VILLEGAS GARCIA <cristianvg9692@gmail.com>
2022-06-29 19:12:40 -0500

    push

---
+++ src/test/java/com/cvg/cya/postulacion/models/repository/MenuRepositoryTest.java
@@ -0,0 +1,15 @@
+package com.cvg.cya.postulacion.models.repository;
+
+import com.cvg.cya.postulacion.models.entity.UserMenu;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class MenuRepositoryTest {
+
+}
